a444dbc0005d37449c26fa39a62c412648b1c57f
lordgeltzdale <lordgeltzdale.devera@hotmail.com>
2018-09-05 16:44:39 +0800

    WAtch Button Text

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Learn to Design and Code</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum nunc vel imperdiet convallis. In sollicitudin leo velit, vitae faucibus elit feugiat a.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch Video</Link>
   </div>
 )
 
