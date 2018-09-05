0b3002a709c7fc524b8bfe52c60a1cba747943d4
lordgeltzdale <lordgeltzdale.devera@hotmail.com>
2018-09-05 16:41:52 +0800

    Added All file

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to Design and Code</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum nunc vel imperdiet convallis. In sollicitudin leo velit, vitae faucibus elit feugiat a.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
